ea4d21177b0da475e947c2ed1877f4b30b0b6ea1
Burmikk <byrmik@gmail.com>
2023-02-28 15:27:28 +0200

    create nav menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './NavBar/NavBar';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <NavBar />
+    // <Routes>
+    //   <Route path="/" element={NavBar} />
+    // </Routes>
   );
 };
